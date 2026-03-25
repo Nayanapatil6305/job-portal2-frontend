edb87f86d26b99f31c12192f937f786fb99a688a
dipalithoravat <dipalithoravat6@gmail.com>
2026-03-25 20:32:54 +0530

    feat: add jobmanagement file

--- src/dashboards/admin/JobManagement.tsx
+++ src/dashboards/admin/JobManagement.tsx
@@ -1,3 +1,17 @@
+// import React from 'react'
+
+// export default function JobManagement() {
+//   return (
+//     <div>JobManagement</div>
+//   )
+// }
+// import React from 'react'
+
+// export default function JobManagement() {
+//   return (
+//     <div>JobManagement</div>
+//   )
+// }
 import React from 'react'
 
 export default function JobManagement() {
